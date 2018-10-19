4405e5953ea26468247f9402ea5b55526bb7acfe
SKULSHADY <anushekprasal@gmail.com>
2018-10-20 02:45:22 +0530

    Material toast icon switch [1/2]
    Also fix the toast layout to center the message and enable it by default.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5482,6 +5482,12 @@ public final class Settings {
         public static final String SHOW_VOLTE_ICON = "volte_icon";
 
         /**
+         * Toast icon
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
