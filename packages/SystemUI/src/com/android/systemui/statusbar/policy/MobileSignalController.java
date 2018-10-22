1d66ad354755a8e989a76b7a5c1771e14c400efd
SagarMakhar <sagarmakhar@gmail.com>
2018-10-22 18:50:29 +0000

    Add VOLTE icon toggle [1/2]
    Change-Id: I28a981517213509c0a40a815c6e40c695fea6c68
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -85,6 +85,7 @@ public class MobileSignalController extends SignalController<
 
     private boolean mShow4gForLte;
     private boolean mDataDisabledIcon;
+    private boolean mVoLTEicon;
 
     private ImsManager mImsManager;
 
@@ -148,6 +149,9 @@ public class MobileSignalController extends SignalController<
             resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.DATA_DISABLED_ICON), false,
                     this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.SHOW_VOLTE_ICON), false,
+                    this, UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -169,6 +173,10 @@ public class MobileSignalController extends SignalController<
 
         mDataDisabledIcon = Settings.System.getIntForUser(resolver,
                 Settings.System.DATA_DISABLED_ICON, 1,
+                UserHandle.USER_CURRENT) == 1;
+
+        mVoLTEicon = Settings.System.getIntForUser(resolver,
+                Settings.System.SHOW_VOLTE_ICON, 1,
                 UserHandle.USER_CURRENT) == 1;
 
         mapIconSets();
@@ -360,7 +368,7 @@ public class MobileSignalController extends SignalController<
     private int getVolteResId() {
         int resId = 0;
 
-        if ( mCurrentState.imsResitered ) {
+        if ( mCurrentState.imsResitered && mVoLTEicon ) {
             resId = R.drawable.ic_volte;
         }
         return resId;
